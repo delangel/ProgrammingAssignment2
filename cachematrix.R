f29659efff4b8a8aba1bbe5ae2563afe1d289c2a
delangel <g_delangel@yahoo.com>
2014-08-22 17:30:29 -0500

    Misc improvements

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
 ## Function to create a matrix while caching its inverse
 ## If inverse has not been created, function will compute it and store it
 
-## Create matrix
+## Create list object that caches inverse of matrix. 
+## Basic syntax:
+##
+## flist = makeCacheMatrix()
+## flist$set(x)
+## fcached = flist$get()
+##
+## finv = cacheSolve(flist) # this computes inverse and caches it
+## finv = cacheSolve(flist) # this returns cached inverse
+## flist$setinv(finv)
+
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -18,7 +28,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## top-level function
+## function to compute and cache the inverse of an object created by the makeCacheMatrix function
+## basic usage:
+##
+## f = cacheSolve(fobj) # fobj was created by makeCacheMatrix as documented in that function
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getinv()
